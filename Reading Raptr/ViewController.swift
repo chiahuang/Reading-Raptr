ca7b6b20031da8c9105cc27c120c42dc57db8bf4
chiahuang <kaylab@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 02:17:38 -0800

    Initial Commit

---
+++ Reading Raptr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reading Raptr
+//
+//  Created by Kay Lab on 11/8/14.
+//  Copyright (c) 2014 Kay Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
