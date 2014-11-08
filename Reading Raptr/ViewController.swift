2541a87e600f6185b3aaed78ed7ab0879081c279
chiahuang <kaylab@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 10:55:26 -0800

    Button Implemented yet there are bug

--- Reading Raptr/ViewController.swift
+++ Reading Raptr/ViewController.swift
@@ -5,16 +5,39 @@
 //  Created by Kay Lab on 11/8/14.
 //  Copyright (c) 2014 Kay Lab. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextView!
+    @IBOutlet weak var button1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func textViewDidChange(textView: UITextView!) { //Handle the text changes here
+        print(textView.text); //the textView parameter is the textView where text was changed
+    }
 
+    
+    @IBAction func buttonSpeakClicked(sender:UIButton)
+    {
+        var mySpeechSynthesizer:AVSpeechSynthesizer = AVSpeechSynthesizer()
+        var myString:String = "This is the phrase to say"
+        var mySpeechUtterance:AVSpeechUtterance = AVSpeechUtterance(string:myString)
+        
+        println("\(mySpeechUtterance.speechString)")
+        println("My string - \(myString)")
+        
+        mySpeechSynthesizer .speakUtterance(mySpeechUtterance)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
