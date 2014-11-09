ced86958720131dc1fe23fce9cca76e29a155b37
AOOE <arsallinger@gmail.com>
2014-11-08 21:57:04 -0800

    Changes to implement NSXMLParser
    Adds function to grab URLs

--- Reading Raptr/ViewController.swift
+++ Reading Raptr/ViewController.swift
@@ -6,9 +6,10 @@
 //  Copyright (c) 2014 Kay Lab. All rights reserved.
 //
 import AVFoundation
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSXMLParserDelegate {
 
     @IBOutlet weak var text1: UITextView!
     @IBOutlet weak var button1: UIButton!
@@ -19,6 +20,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // MARK: NSXMLParserDelegate
+    
+    func parser(parser: NSXMLParser, foundElementDeclarationWithName elementName: String, model: String) {
+
+    }
     
     func textViewDidChange(textView: UITextView!) { //Handle the text changes here
         print(textView.text); //the textView parameter is the textView where text was changed
@@ -27,22 +33,35 @@ class ViewController: UIViewController {
     
     @IBAction func buttonSpeakClicked(sender:UIButton)
     {
+        
+        var text : String = text1.text
         var mySpeechSynthesizer:AVSpeechSynthesizer = AVSpeechSynthesizer()
-        var myString:String = "This is the phrase to say"
+        var myString:String = "\(text)"
         var mySpeechUtterance:AVSpeechUtterance = AVSpeechUtterance(string:myString)
         
-        println("\(mySpeechUtterance.speechString)")
-        println("My string - \(myString)")
         
-        mySpeechSynthesizer .speakUtterance(mySpeechUtterance)
+        if let url = NSURL(string: "http://rss.cnn.com/rss/cnn_topstories.rss"){
+            let request = NSURLRequest(URL: url)
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
+                (response, data, error) in
+                
+                // let myString = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                // println (myString)
+                
+                let parser = NSXMLParser(data: data)
+                
+                parser.delegate = self
+                
+                parser.parse()
+            }
+        }
+        //Commented out for test
+        //println("\(mySpeechUtterance.speechString)")
+        //println("My string - \(myString)")
+        
+        mySpeechSynthesizer.speakUtterance(mySpeechUtterance)
     }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
